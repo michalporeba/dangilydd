d43427340a1451ca1e080648575bfb186096778c
Michał Poręba <michalporeba@gmail.com>
2022-01-04 20:39:48 +0000

    starting flutter project

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # People and their Abilitis in Use (PATAIU)
 
 A tool to manage events requiring individuals with diverse skills and abilities.
+
+>It is one of several projects which could solve real problems and get used by real users, but their real objective is to help me learn [dart](https://dart.dev) and [flutter](https://flutter.dev). The other projects are: [flow](https://github.com/michalporeba/flow), [stuffed](https://github.com/michalporeba/stuffed) and [ticks](https://github.com/michalporeba/stuffed). 
+
+
+&nbsp;
+## Problem domain
+
+Sometime, to run an event one may need a group of suitably qualified individuals. Their training, qualifications and suitability have to managed. 
+
+&nbsp;
+## The result
+So far it looks as unimpressive as this:
+
+![](./docs/images/first.png)
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Deep Flow',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: const MyHomePage(title: 'PATAIU'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text('to be continued ...', style: Theme.of(context).textTheme.headline4)
+      ),
+    );
+  }
+}
