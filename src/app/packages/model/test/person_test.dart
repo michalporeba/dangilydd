24a90273ce21a8114eed74cb1232fc2ff070dcaf
Michał Poręba <michalporeba@gmail.com>
2023-01-26 21:28:25 +0000

    consolidating tests for better readability

--- src/app/packages/model/test/person_test.dart
+++ src/app/packages/model/test/person_test.dart
@@ -4,27 +4,21 @@ import 'package:model/person.dart';
 import 'package:model/facts.dart';
 
 void main() {
-  test('a newly created person has no events', () {
+  test('attended events are counted', () {
     final sut = Person();
     expect(sut.getEventsSummary().count, 0);
-  });
-
-  test('an event attendance can be added', () {
-    final sut = Person();
     sut.addFact(const EventAttendance(duration: 0));
     expect(sut.getEventsSummary().count, 1);
-  });
-
-  test('multiple event attendances can be added', () {
-    final sut = Person();
-    sut.addFact(const EventAttendance(duration: 0));
     sut.addFact(const EventAttendance(duration: 0));
     expect(sut.getEventsSummary().count, 2);
   });
 
-  test('events time is added up', (){
+  test('attended events time is added up', (){
     final sut = Person();
+    expect(sut.getEventsSummary().count, 0);
     sut.addFact(const EventAttendance(duration: 45));
     expect(sut.getEventsSummary().duration, 45);
+    sut.addFact(const EventAttendance(duration: 30));
+    expect(sut.getEventsSummary().duration, 75);
   });
 }
