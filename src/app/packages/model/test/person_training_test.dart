56b3377f37379caf141b78f54f475fa70976af8f
Michał Poręba <michalporeba@gmail.com>
2023-01-27 22:55:03 +0000

    splitting test files

---
+++ src/app/packages/model/test/person_training_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:model/person.dart';
+import 'package:model/facts.dart';
+
+void main() {
+  test('attended training is counted', (){
+    final sut = Person();
+    expect(sut.getTrainingSummary().count, 0);
+    sut.addFact(const TrainingAttendance(duration: 0));
+    expect(sut.getTrainingSummary().count, 1);
+    sut.addFact(const TrainingAttendance(duration: 0));
+    expect(sut.getTrainingSummary().count, 2);
+    // check if other facts are not included
+    sut.addFact(const EventAttendance(duration: 15));
+    expect(sut.getTrainingSummary().count, 2);
+  });
+
+  test('attended training time is added up', () {
+    final sut = Person();
+    expect(sut.getTrainingSummary().duration, 0);
+    sut.addFact(const TrainingAttendance(duration: 90));
+    expect(sut.getTrainingSummary().duration, 90);
+    sut.addFact(const TrainingAttendance(duration: 13));
+    expect(sut.getTrainingSummary().duration, 103);
+    // check if other facts are not included
+    sut.addFact(const EventAttendance(duration: 15));
+    expect(sut.getTrainingSummary().duration, 103);
+  });
+}
